f04d6903cce9f519f183398ff5a7046676af30f5
israelmathusse <israelmathusse451@gmail.com>
2021-10-23 15:04:53 +0100

    01-primeiros scripts do cadastro

---
+++ cadastro.php
@@ -0,0 +1,53 @@
+<?php
+require_once 'usuario.php';
+$u= new usuarios;
+ ?>
+
+
+<?php
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+    $nome= addslashes($_POST['nome']);
+	$senha=addslashes($_POST['senha']);
+	$confsenha=addslashes($_POST['confsenha']);
+$u->conectar("sis_login", "localhost", "root", "");
+if(!empty($nome) && !empty($senha)){
+    if($u->msgerro==""){
+        if($senha==$confsenha){
+            if($u->cadastrar($nome, $senha)){
+				echo"cadastrado com sucesso";
+            }else{
+                echo "nome e senha ja cadastrados, tente outro ou faca login";
+            }
+        }else{
+            echo "Digitou a senha erradamente, reveja";
+        }
+    }else{
+        echo "erro:". $u->msgerro;
+    }
+}else{
+    echo "Preencha todos os campos";
+}}
+/*
+
+	
+	
+	
+		
+	
+			if($u->cadastrar($id_usuario,$nome,$telefone, $email, $senha)){
+				echo"cadastrado com sucesso";
+			}else{
+				echo "email ja cadastrado";
+			}
+		}else{
+			echo"digitou senha errada, reveja";
+		}
+	}else{
+		echo "erro:". $u->msgerro;
+	}
+	}else{
+		echo "preencha todos os campos";
+	}}
+*/
+
+     ?>
